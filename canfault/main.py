a272b75b791689f7806d35512485fcc4ea967374
kalle stenzelius <kalmaster@localhost.localdomain>
2021-11-17 13:04:57 +0100

    seting up a channel

---
+++ canfault/main.py
@@ -0,0 +1,24 @@
+from canlib import canlib, Frame
+from canlib.canlib import ChannelData
+
+def setUpChannel(channel=0,
+                 openFlags=canlib.canOPEN_ACCEPT_VIRTUAL,
+                 bitrate=canlib.canBITRATE_500K,
+                 bitrateFlags=canlib.canDRIVER_NORMAL):
+    ch = canlib.openChannel(channel, openFlags)
+    print("Using channel: %s, EAN: %s" % (ChannelData(channel).channel_name,
+                                          ChannelData(channel).card_upc_no))
+    ch.setBusOutputControl(bitrateFlags)
+    ch.setBusParams(bitrate)
+    ch.busOn()
+    return ch
+
+def tearDownChannel(ch):
+    ch.busOff()
+    ch.close()
+
+
+if __name__ == '__main__':
+    ch = setUpChannel(channel=0)
+    
+    tearDownChannel(ch)
