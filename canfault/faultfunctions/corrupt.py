d042b97dd91338e72b2a14fec65d5099b00ee172
Mattias Ivarsson <mattias_ivarson@hotmail.com>
2021-11-18 10:09:17 +0100

    Quick fix params to corrupt temp function

--- canfault/faultfunctions/corrupt.py
+++ canfault/faultfunctions/corrupt.py
@@ -1,7 +1,7 @@
 from canlib import canlib, Frame
 from canlib.canlib import ChannelData
 
-def corrupt_frame(frame):
+def corrupt_frame(frame, params = []):
     print(frame)
     #print(dir(frame))
     frame.data = "derp"
