b6ba07f3af18b731df7337bc27b484cbd9be957e
kalle stenzelius <kalmaster@localhost.localdomain>
2021-11-18 11:15:32 +0100

    extra dup

--- canfault/faultfunctions/duplicate.py
+++ canfault/faultfunctions/duplicate.py
@@ -1,2 +1,10 @@
-def funk():
-    print("hej")
+from canlib import canlib, Frame
+from environment import transciever
+
+def duplicate(frame, params = []):
+    return [frame,frame]
+
+
+if __name__ == '__main__':
+    fr = transciever.framefactory.random()
+    print(duplicate(fr))
