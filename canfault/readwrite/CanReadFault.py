fc5f52980c28c7482ae154b43be99a3d763e6578
dooote <wowdooote@live.se>
2021-11-18 01:12:35 -0800

    Squashed commit of the following:
    commit 37a298d84f1fc47e9ef202a0d1bd21e5ee3594dd
    Author: dooote <wowdooote@live.se>
    Date:   Thu Nov 18 01:12:13 2021 -0800
    
        Reformatted the comment
    
    commit 75ba7276fff7e04bf696bdf90097ed2ae27711b8
    Merge: 077325a 17d28e3
    Author: dooote <wowdooote@live.se>
    Date:   Thu Nov 18 01:01:56 2021 -0800
    
        Merge branch 'master' into Read
    
    commit 077325add612b4348621adc7ab54b865d6ee7119
    Author: dooote <wowdooote@live.se>
    Date:   Thu Nov 18 01:01:24 2021 -0800
    
        added params functionality
    
    commit dc8602b4d096c2a8ea8d4cc9f852c1aff6fa1b39
    Author: dooote <wowdooote@live.se>
    Date:   Thu Nov 18 00:23:49 2021 -0800
    
        used the channel methods from main instead of creating my own
    
    commit 336e12e4c21ab356fb57a6f64ff6f99ce0f7eb62
    Merge: 803e2f9 a8cf40a
    Author: dooote <wowdooote@live.se>
    Date:   Thu Nov 18 00:22:24 2021 -0800
    
        Merge branch 'master' into Read
    
    commit 803e2f94be4dab090c442272107a4d14d7ff4c69
    Author: dooote <wowdooote@live.se>
    Date:   Thu Nov 18 00:21:54 2021 -0800
    
        asd
    
    commit 32449cc844063a89a78f23e8aa3b65a7173c6691
    Merge: 7c80fa5 d0bcf50
    Author: dooote <wowdooote@live.se>
    Date:   Thu Nov 18 00:07:06 2021 -0800
    
        Merge branch 'master' into Read
    
    commit 7c80fa5551097e809e54df50280895b1c2142a6a
    Author: dooote <wowdooote@live.se>
    Date:   Thu Nov 18 00:05:00 2021 -0800
    
        added functionality to input a frame to read
    
    commit 0472de02603b13a6122e1056506c4930c7ffea2e
    Merge: 3e41ca4 3763874
    Author: dooote <wowdooote@live.se>
    Date:   Wed Nov 17 06:30:48 2021 -0800
    
        Merge branch 'master' into Read
    
    commit 3e41ca4092421e875279e2543f99e89278d1c158
    Author: dooote <wowdooote@live.se>
    Date:   Wed Nov 17 06:28:45 2021 -0800
    
        Cleaned the code

--- canfault/readwrite/CanReadFault.py
+++ canfault/readwrite/CanReadFault.py
@@ -1,9 +1,10 @@
 from canlib import canlib
 
-# Used to read from the bus if no frame is supplied.
-# Used to pretend to read a frame from the bus if a frame is supplied.
-# May be used when you want to inject faults in only one of the components
-# connected to a bus with multiple components connected to it.
+""" Used to read from the bus if no frame is supplied.
+ Used to pretend to read a frame from the bus if a frame is supplied.
+ May be used when you want to inject faults in only one of the components
+ connected to a bus with multiple components connected to it. 
+"""
 def read(channel, func, frame = None, params = []):
     while True:
         try:
