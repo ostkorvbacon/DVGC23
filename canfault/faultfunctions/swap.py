64523acbb16bb316e489f42b0efc9ede28b15517
dooote <wowdooote@live.se>
2021-11-18 06:56:44 -0800

    Squashed commit of the following:
    commit 20fdfa2ce91d5cb7b8d71cef603f15b6c3e23445
    Merge: 944c3d9 d682016
    Author: dooote <wowdooote@live.se>
    Date:   Thu Nov 18 06:56:29 2021 -0800
    
        Merge branch 'master' into Swap
    
    commit 944c3d9838ace49c16d0860e6c2b3a9b0b4767ad
    Author: dooote <wowdooote@live.se>
    Date:   Thu Nov 18 06:56:19 2021 -0800
    
        solved returning two frames with a list
    
        Note this may need to change depending on how the costumer wants to use the program.
    
    commit c3b08cbc01989a7de5d10e3ba11732084884aca0
    Merge: c7efc02 fe96c88
    Author: dooote <wowdooote@live.se>
    Date:   Thu Nov 18 02:44:10 2021 -0800
    
        Merge branch 'master' into Swap
    
    commit c7efc024107762c72645556fcf69e7e16739bed7
    Author: dooote <wowdooote@live.se>
    Date:   Thu Nov 18 02:43:36 2021 -0800
    
        implementing swap...

---
+++ canfault/faultfunctions/swap.py
@@ -0,0 +1,17 @@
+_frame = None
+_stored = 0
+
+def set_stored(frame):
+    if(_stored == 1):
+        _stored = 0
+        return _frame
+    else:
+        _frame = frame
+        _stored = 1
+        return None
+
+def swap(frame, params):
+    if(set_stored is None)
+        return set_stored(frame)
+    else:
+        return set_stored(frame), frame
--- canfault/readwrite/CanWriteFault.py
+++ canfault/readwrite/CanWriteFault.py
@@ -1,4 +1,8 @@
 def write(channel, func, frame, params = []):
     frame_fault = func(frame, params)
-    if(frame_fault != None):
-        channel.write(frame_fault)
+    if(frame_fault is not None):
+        if(isinstance(frame_fault, list)):
+            for single_frame in frame_fault:
+                channel.write(single_frame)
+        else:
+            channel.write(frame_fault)
