dc8602b4d096c2a8ea8d4cc9f852c1aff6fa1b39
dooote <wowdooote@live.se>
2021-11-18 00:23:49 -0800

    used the channel methods from main instead of creating my own

--- canfault/readwrite/TestRead.py
+++ canfault/readwrite/TestRead.py
@@ -8,29 +8,13 @@ import CanReadFault
 import CanWriteFault
 from main import setUpChannel, tearDownChannel
 
-def setUpChannel(channel=0,
-                 openFlags=canlib.canOPEN_ACCEPT_VIRTUAL,
-                 bitrate=canlib.canBITRATE_500K,
-                 bitrateFlags=canlib.canDRIVER_NORMAL):
-    ch = canlib.openChannel(channel, openFlags)
-    print("Using channel: %s, EAN: %s" % (ChannelData(channel).channel_name,
-                                          ChannelData(channel).card_upc_no))
-    ch.setBusOutputControl(bitrateFlags)
-    ch.setBusParams(bitrate)
-    ch.busOn()
-    return ch
-
-def tearDownChannel(ch):
-    ch.busOff()
-    ch.close()
-
 def do_nothing(frame):
     return frame
 
 def main():
     print("Setting up channel!")
-    channel0 = main.setUpChannel()
-    channel1 = main.setUpChannel(1)
+    channel0 = setUpChannel()
+    channel1 = setUpChannel(1)
     print("Writing frame!")
     frame = Frame(
         id_=100,
@@ -40,8 +24,8 @@ def main():
     CanWriteFault.write(channel1, do_nothing, frame)
     print("Reading on channel!")
     CanReadFault.read(channel0, print)
-    main.tearDownChannel(channel0)
-    main.tearDownChannel(channel1)
+    tearDownChannel(channel0)
+    tearDownChannel(channel1)
 
 
 main()
