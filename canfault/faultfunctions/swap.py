c7efc024107762c72645556fcf69e7e16739bed7
dooote <wowdooote@live.se>
2021-11-18 02:43:36 -0800

    implementing swap...

---
+++ canfault/faultfunctions/swap.py
@@ -0,0 +1,18 @@
+_frame = None
+_stored = 0
+
+def set_stored(frame):
+    if(_stored == 1):
+        _stored = 0
+        return _frame
+    else:
+        _frame = frame
+        _stored = 1
+        return None
+
+def swap(frame, params):
+    if(set_stored is None)
+        return set_stored(frame)
+    else:
+        params[0].write(set_stored)
+        return frame
