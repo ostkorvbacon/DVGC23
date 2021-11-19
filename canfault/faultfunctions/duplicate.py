b36158dd4cdfe0d78336ff9e268b862d0c1e27f5
kalle stenzelius <kalmaster@localhost.localdomain>
2021-11-19 10:26:55 +0100

    duplicate

--- canfault/faultfunctions/duplicate.py
+++ canfault/faultfunctions/duplicate.py
@@ -1,10 +1,4 @@
 from canlib import canlib, Frame
-from environment import transciever
 
 def duplicate(frame, params = []):
     return [frame,frame]
-
-
-if __name__ == '__main__':
-    fr = transciever.framefactory.random()
-    print(duplicate(fr))
