045c197d612a4017a2ea5ed1bd84f7c37b1c51e8
Ali <ali.ferzali@gmail.com>
2021-11-19 11:01:53 +0100

    Test 3

--- canfault/faultfunctions/DelayFrames.py
+++ canfault/faultfunctions/DelayFrames.py
@@ -1,6 +1,7 @@
 from canlib import canlib, Frame
 # Mera from, kanske env eller canlib? Import?
-def DelayFrames(Frames, Delay):
-    print("Ali säger Wazaammm")
+def DelayFrames(Frames, Delay): 
+    return  
 
-
+#CONNECTION_DELAY = 5 
+#Overload är gammalt ? Finns det något nyare?
