5f11e8c1aa9a9c3eb8b256bc806dd9145b27b5c1
Ali <ali.ferzali@gmail.com>
2021-12-01 10:03:17 +0100

    Delay update

--- canfault/faultfunctions/delay.py
+++ canfault/faultfunctions/delay.py
@@ -1,9 +1,6 @@
+from canlib import canlib, Frame, frame 
 import time
-from canlib import canlib, Frame
-# Mera from, kanske env eller canlib? Import?
-def delay(frame, delay = 0): 
-    time.sleep(1)
+'''Skickar arrayer med parameter, kallar på funktionen'''
+def Delay(frame, params =[]): 
+    time.sleep(params[0])
     return frame
-
-#CONNECTION_DELAY = 5 
-#Overload är gammalt ? Finns det något nyare?
