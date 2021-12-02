a7e9fb1b30b7776f2ae5364c9e3db83522d076ed
dooote <wowdooote@live.se>
2021-12-02 04:25:07 -0800

    fixed an issue with delay and added tests for delay
    Added tests to cover 100% of delay and fixed an issue that caused the program to crash if you called delay without an input on params.

--- canfault/tests/test_delay.py
+++ canfault/tests/test_delay.py
@@ -1,8 +1,42 @@
 import unittest
 from canlib import canlib, Frame
 from faultfunctions import faultfunction
+from timeit import default_timer as timer
+
+class TestSwap(unittest.TestCase):
+
+    def setUp(self):
+        self.frame1 = Frame(
+            id_=0,
+            data=[0],
+            flags=canlib.MessageFlag.EXT
+        )
+        self.frame2 = Frame(
+            id_=1,
+            data=[1],
+            flags=canlib.MessageFlag.EXT
+        )
+    
+    def test_delay_one_sec(self):
+        start = timer()
+        ret_frame = delay(self.frame1, [1])
+        time = timer() - start
+        self.assertGreaterEqual(time, 1)
+        self.assertLess(time, 2)
+        self.assertEqual(ret_frame, self.frame1)
+
+    def test_delay_no_param(self):
+        start = timer()
+        ret_frame = delay(self.frame1)
+        time = timer() - start
+        self.assertGreaterEqual(time, 0.1)
+        self.assertLess(time, 1)
+        self.assertEqual(ret_frame, self.frame1)
+
+    
+
 if __name__ == '__main__': # pragma: no cover
-    from faultfunctions import faultfunction
+    from faultfunctions import delay
     unittest.main()
 else:
     from faultfunctions.faultfunction import delay
