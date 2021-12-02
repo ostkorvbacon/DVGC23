202e3a2571cb092d8fc2c6072b22b0ef774abe3f
dooote <wowdooote@live.se>
2021-12-02 05:33:14 -0800

    Added some docstrings to faultfunctions.py
    docstrings not added to bit_filler() and corrupt().

--- canfault/faultfunctions/faultfunction.py
+++ canfault/faultfunctions/faultfunction.py
@@ -6,6 +6,30 @@ import time
 import logging
 import random
 
+_frame = None
+_stored = 0
+
+def set_stored(frame):
+    """Stores a Frame for swap, returns the frame if a frame is stored otherwise None.
+ 
+    :param frame: canlib Frame to be stored
+    :type frame: canlib.Frame
+    
+    :rtype: canlib.Frame or None
+    :return: frame or None
+    """
+    global _stored
+    global _frame
+    if(_stored == 1):
+        _stored = 0
+        ret = _frame
+        _frame = None
+        return ret, 1
+    else:
+        _frame = frame
+        _stored = 1
+        return None, 0
+
 def bit_filler(frame_as_bits = bitarray("0000 0000")):
     bit_filler = bitarray(endian='big')
     bit_filler = util.zeros(8)
@@ -39,6 +63,16 @@ def corrupt(frame, params = []):
 
 
 def delay(frame, params = []):
+    """Delays a Frame by a number of seconds, returns the Frame.
+ 
+    :param frame: canlib Frame to be delayed
+    :type frame: canlib.Frame
+    :param params: first element is time to delay in seconds, defaults to 0.1
+    :type params: list with first element as int, optional
+    
+    :rtype: canlib.Frame
+    :return: frame
+    """
     if(len(params) == 0):
         t = 0.1
     else:
@@ -48,6 +82,16 @@ def delay(frame, params = []):
 
 
 def duplicate(frame, params = []):
+    """Returns a number of copies of a Frame.
+ 
+    :param frame: canlib Frame to be copied
+    :type frame: canlib.Frame
+    :param params: first element is number of copies, defaults to 2
+    :type params: list with first element as int, optional
+    
+    :rtype: list
+    :return: [frame * params[0]]
+    """
     f_list = []
     if(len(params) == 0):
         amount = 2
@@ -57,23 +101,17 @@ def duplicate(frame, params = []):
         f_list.append(frame)
     return f_list
 
-_frame = None
-_stored = 0
-
-def set_stored(frame):
-    global _stored
-    global _frame
-    if(_stored == 1):
-        _stored = 0
-        ret = _frame
-        _frame = None
-        return ret, 1
-    else:
-        _frame = frame
-        _stored = 1
-        return None, 0
-
 def swap(frame, params = []):
+    """Swaps the order of two Frames and returns them in a list.
+ 
+    :param frame: canlib Frame to be swaped
+    :type frame: canlib.Frame
+    :param params: not used, defaults to []
+    :type params: optional
+    
+    :rtype: list
+    :return: [frame, old_frame]
+    """
     old_frame, stored = set_stored(frame)
     if(stored == 1):
         return [frame, old_frame]
@@ -82,6 +120,16 @@ def swap(frame, params = []):
 
 
 def insert(frame, params =[]):
+    """Inserts a Frame with random id and data.
+ 
+    :param frame: canlib Frame to follow the inserted frame
+    :type frame: canlib.Frame
+    :param params: not used, defaults to []
+    :type params: optional
+    
+    :rtype: list
+    :return: [new_frame, frame]
+    """
     frame_id = random.randint(0, 1023)
     data = random.randint(0, 255)
     new_frame = Frame(frame_id, [data], flags = canlib.MessageFlag.EXT)
