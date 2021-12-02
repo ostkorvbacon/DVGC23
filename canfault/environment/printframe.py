b4f2386ad39913d33ecf38788f6f2a43dd80fca5
kalle stenzelius <kalmaster@localhost.localdomain>
2021-12-02 14:08:11 +0100

    hex

--- canfault/environment/printframe.py
+++ canfault/environment/printframe.py
@@ -5,7 +5,7 @@ def print_frame(frame):
     if not isinstance(frame, Frame):
         raise TypeError("print_frame requires type Frame")
     else:
-        print("id_ = {}".format(frame.id))
+        print("id_ = {}".format(hex(frame.id)))
         print("data = \\x", end = "")
         print('\\x'.join('{:02x}'.format(d) for d in frame.data))
         print("flags = {}\n".format(frame.flags))
