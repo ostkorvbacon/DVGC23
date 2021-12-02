59aa44c002252ca600ecce1f4043fe579bfa81b5
ostkorvbacon <55699954+ostkorvbacon@users.noreply.github.com>
2021-12-02 02:07:20 -0800

    fixar

--- canfault/faultfunctions/faultfunction.py
+++ canfault/faultfunctions/faultfunction.py
@@ -3,8 +3,8 @@ from canlib.canlib import ChannelData
 from bitarray import bitarray
 from bitarray import util
 import time
-<<<<<<< HEAD
 import logging
+import random
 
 def bit_filler(frame_as_bits):
     bit_filler = bitarray(endian='big')
@@ -12,9 +12,6 @@ def bit_filler(frame_as_bits):
     while frame_as_bits.count(0) + frame_as_bits.count(1) < 64:
         frame_as_bits.extend(bit_filler)
     return frame_as_bits
-=======
-import random
->>>>>>> 624a1a66d8f30d2995156a0d97a6df707bedb5fe
 
 def corrupt(frame, params = []):
     start = params[0]
@@ -71,12 +68,10 @@ def swap(frame, params = []):
         return [frame, old_frame]
     else:
         return old_frame
-<<<<<<< HEAD
-=======
+
 
 def insert(frame, params =[]):
     frame_id = random.randint(0, 1023)
     data = random.randint(2047, 8191)
     new_frame = Frame(frame_id, data, flags = canlib.MessageFlag.EXT)
     return[new_frame,frame]
->>>>>>> 624a1a66d8f30d2995156a0d97a6df707bedb5fe
