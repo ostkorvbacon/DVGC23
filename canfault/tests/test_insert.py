779badf95e064d909c6ffc090e352761e308d974
kalle stenzelius <kalmaster@localhost.localdomain>
2021-12-02 12:09:28 +0100

    test for insert

---
+++ canfault/tests/test_insert.py
@@ -0,0 +1,20 @@
+import unittest
+from faultfunctions import faultfunction
+from canlib import canlib, Frame
+class TestDuplicate(unittest.TestCase):
+    
+    def setUp(self):
+        self.frame = Frame(23, [22], flags = canlib.MessageFlag.EXT)
+        pass
+    def tearDown(self):
+        pass
+
+    def test_insert(self):
+        a = False
+        s = faultfunction.insert(self.frame)
+        new_frame = s[0]
+        old_frame = s[1]
+        self.assertEqual(old_frame,self.frame)
+        if(new_frame.__class__ == Frame):
+            a = True
+        self.assertEqual(a,True)
