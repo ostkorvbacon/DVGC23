029898db9bca070a730d7b3b1ba87d798fadfeb8
dooote <wowdooote@live.se>
2021-12-03 01:43:26 -0800

    added docstrings to read and write

--- canfault/readwrite/writefault.py
+++ canfault/readwrite/writefault.py
@@ -1,6 +1,18 @@
 from canlib import canlib, Frame
 
 def write(channel, frame, func = None, params = []):
+    """Writes Frames to a channel and optionally runs them through a function.
+ 
+    :param channel: the channel from whtch the frames are read
+    :type channel: canlib.Channel
+    :param frame: the frame to send on channel
+    :type frame: canlib.Frame
+    :param func: the functions to run the Frames through, defaults to None
+    :type func: callable, optional
+    :param params: list of parametes to pass to func, defaults to []
+    :type params: list, optional
+    :raises TypeError: if parameters are of the wrong type
+    """
     if(not isinstance(frame, Frame)):
         if(not isinstance(frame, list)):
             raise TypeError("The passed frame is not a canlib Frame!")
