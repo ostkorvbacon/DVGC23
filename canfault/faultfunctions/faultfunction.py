7b29e0f5b768ece39d6c0bc80672c91be23ec054
dooote <wowdooote@live.se>
2021-12-03 00:28:16 -0800

    added docstrings to the remaining functions in faultfunctions.py

--- canfault/faultfunctions/faultfunction.py
+++ canfault/faultfunctions/faultfunction.py
@@ -31,6 +31,14 @@ def set_stored(frame):
         return None, 0
 
 def bit_filler(frame_as_bits = bitarray("0000 0000")):
+    """Fills a bitarray with 64 bits, returns the bitarray.
+ 
+    :param frame_as_bits: the bitarray to be filled, defaults to bitarray("0000 0000")
+    :type frame: bitarray, optional
+    
+    :rtype: bitarray
+    :return: frame_as_bits
+    """
     bit_filler = bitarray(endian='big')
     bit_filler = util.zeros(8)
     while frame_as_bits.count(0) + frame_as_bits.count(1) < 64:
@@ -38,6 +46,16 @@ def bit_filler(frame_as_bits = bitarray("0000 0000")):
     return frame_as_bits
 
 def corrupt(frame, params = []):
+    """Bitflipps the data of a Frame, returns the Frame.
+ 
+    :param frame: canlib Frame to be bitflipped
+    :type frame: canlib.Frame
+    :param params: params[0] is startbit and params[1] is number of bits to be flipped, defaults to []
+    :type params: list with first and second elements as ints, optional
+    
+    :rtype: canlib.Frame
+    :return: frame
+    """
     start = params[0]
     lenght = params[1]
     if start < 0 or lenght + start > 64:
