c399b7e1360ad93905971b33ca9c7761c339b569
kalle stenzelius <kalmaster@localhost.localdomain>
2021-12-09 11:48:56 +0100

    pep8 readwrite

--- canfault/readwrite/readfault.py
+++ canfault/readwrite/readfault.py
@@ -1,24 +1,29 @@
 from canlib import canlib, Frame
 from typing import Callable
 
+
 """ Used to read from the bus if no frame is supplied.
  Used to pretend to read a frame from the bus if a frame is supplied.
  May be used when you want to inject faults in only one of the components
- connected to a bus with multiple components connected to it. 
+ connected to a bus with multiple components connected to it.
 """
-def read(channel, func = None, frame = None, params = []):
-    """Reads Frames from a channel and optionally runs them through a function, returns the Frame.
- 
+
+
+def read(channel, func=None, frame=None, params=[]):
+    """Reads Frames from a channel and optionally
+    runs them through a function, returns the Frame.
+
     :param channel: the channel from whtch the frames are read
     :type channel: canlib.channel.Channel
     :param func: the functions to run the Frames through, defaults to None
     :type func: callable, optional
-    :param frame: if provided, this frame will be used instead of reading from channel, defaults to None
+    :param frame: if provided, this frame
+    will be used instead of reading from channel, defaults to None
     :type frame: canlib.Frame, optional
     :param params: list of parametes to pass to func, defaults to []
     :type params: list, optional
     :raises TypeError: if parameters are of the wrong type
-    
+
     :rtype: canlib.Frame, None
     :return: frame, None
     """
@@ -39,6 +44,3 @@ def read(channel, func = None, frame = None, params = []):
             return func(frame, params)
         except (canlib.canNoMsg) as ex:
             return None
-    
-
-            
