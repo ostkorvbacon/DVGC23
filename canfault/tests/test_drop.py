a7dcb0bc6365e98f1fc36e389dc2e1c49fa45d90
kalle stenzelius <kalmaster@localhost.localdomain>
2021-12-15 11:34:26 +0100

    tests

---
+++ canfault/tests/test_drop.py
@@ -0,0 +1,17 @@
+import unittest
+from faultfunctions import faultfunction
+from canlib import canlib, Frame
+
+
+class TestDuplicate(unittest.TestCase):
+    """Tests the faultfuncton.drop function."""
+    def setUp(self):
+        self.frame = Frame(23, [22], flags=canlib.MessageFlag.EXT)
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_drop(self):
+        r = faultfunction.drop(self.frame)
+        self.assertEqual(r,None)
